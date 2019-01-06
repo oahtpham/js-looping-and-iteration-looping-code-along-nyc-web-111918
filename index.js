53d464f21bc614d8bc13b0b9c79258280575c103
oahtpham <phamthaop.9@gmail.com>
2019-01-06 14:00:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+
+function printBadges(array) {
+  for (i = 0; i < array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`)
+  }
+  return array
+}
+
+function tailsNeverFails() {
+  let i = 0;
+  while (Math.random() >= 0.5) {
+    i++
+  }
+  return `You got ${i} tails in a row!`
+}
